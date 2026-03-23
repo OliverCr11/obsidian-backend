71f86a84e1501b596ab78deccf939a8309c68dfc
OLiverCr <stalincriollo11@gmail.com>
2026-03-23 08:13:17 -0400

    feat: create api view to process and save customer orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Order
+from .serializers import OrderSerializer
 
-# Create your views here.
+class CreateOrderView(generics.CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
