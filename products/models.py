32cfc636cdaab53850a58e3ff0936e66002cd839
OLiverCr <stalincriollo11@gmail.com>
2026-03-24 07:47:39 -0400

    refactor: removed redundant image field and implemented gallery serializer

--- products/models.py
+++ products/models.py
@@ -49,7 +49,6 @@ class Glove(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
-    image = models.ImageField(upload_to='products/')
     size = models.CharField(max_length=2, choices=SIZE_CHOICES, default='M')
     created_at = models.DateTimeField(auto_now_add=True)
 
--- products/serializers.py
+++ products/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 from .models import Glove, Category, ProductImage
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image', 'is_primary']
+
 class GloveSerializer(serializers.ModelSerializer):
     category = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    images = serializers.SerializerMethodField()
+    images = ProductImageSerializer(many=True, read_only=True)
     
     class Meta:
         model = Glove
         fields = '__all__'
-
-    def get_images(self, obj):
-        request = self.context.get('request')
-        if not request:
-            return [img.image.url for img in obj.images.all()]
-        return [request.build_absolute_uri(img.image.url) for img in obj.images.all()]
